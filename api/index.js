2ecae5b7eabaa460c9cc78ca86a586542cb9d522
Achille Maboa <achille.maboa@icloud.com>
2023-11-09 18:36:14 +0100

    creation test api route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import connectDB from '../Database/connectDB.js';
+import userRouter from './routes/user.route.js';
 const app = express ();
 const port = process.env.PORT || 3000;
-const DATABASE_URL = process.env.DATABASE_URL | 'mongodb://127.0.0.1:27017/test' 
+const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/test' 
 
 connectDB(DATABASE_URL);
 
 app.listen(port, () => console.log (`server listening on port ${port}`));
+
+
+
+app.use("/api/user",userRouter)
