2ad6fe0f15699b7b31252e44c7613570a05492dc
Achille Maboa <achille.maboa@icloud.com>
2023-11-09 18:00:54 +0100

    creation user model

--- api/index.js
+++ api/index.js
@@ -1,5 +1,9 @@
 import express from 'express'
-
+import connectDB from '../Database/connectDB.js';
 const app = express ();
+const port = process.env.PORT || 3000;
+const DATABASE_URL = process.env.DATABASE_URL | 'mongodb://127.0.0.1:27017/test' 
 
-app.listen(3000, () => console.log ('server up!'))
+connectDB(DATABASE_URL);
+
+app.listen(port, () => console.log (`server listening on port ${port}`));
